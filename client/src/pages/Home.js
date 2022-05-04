b74db2b4f9d9ae08245adcc21c3ea176e1ab7e83
robcabobb <robcabobb@gmail.com>
2022-05-04 15:34:06 -0400

    added landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import ProductList from "../components/ProductList";
-import CategoryMenu from "../components/CategoryMenu";
-import Cart from "../components/Cart";
+import LandingPage from "../components/LandingPage";
 
 const Home = () => {
   return (
     <div className="container">
-      <CategoryMenu />
-      <ProductList />
-      <Cart />
+      <LandingPage />
     </div>
   );
 };
---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      primary: '#EE9789'
+    },
+   
+  },
+  plugins: [],
+}
