148bfabf81980a957e0094c18e157cb77656d91f
robcabobb <robcabobb@gmail.com>
2022-05-05 00:12:06 -0400

    added photos

--- client/src/pages/Merch.js
+++ client/src/pages/Merch.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Cart from "../components/Cart";
-import ProductItem from "../components/ProductItem";
 import ProductList from "../components/ProductList";
 
 const Merch = () => {
 	return (
 		<div>
-			<ProductItem />
+			
 			<ProductList />
 			<Cart />
 		</div>
