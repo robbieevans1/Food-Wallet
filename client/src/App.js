ee3aa017cd16b35f237bc3bc9dcb797cad76fdf3
robcabobb <robcabobb@gmail.com>
2022-05-04 22:06:22 -0400

    landing page close to correct

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,14 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import Home from './pages/Home';
-
+import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
+import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
-import Success from './pages/Success'
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -50,6 +50,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
+              <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+import LandingPage from "../components/LandingPage";
 
 const Home = () => {
   return (
-    <div className="container">
-      
-    </div>
+    
+      < LandingPage />
+    
   );
 };
 
