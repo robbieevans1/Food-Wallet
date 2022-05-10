42af72a01b513e8313fdfa9f3e63cb04f7f15c1e
robcabobb <robcabobb@gmail.com>
2022-05-09 20:57:24 -0400

    created meal list component

--- client/src/pages/FoodData.js
+++ client/src/pages/FoodData.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Modal from '../components/FoodData/Modal'
 
 const FoodData = () => {
   return (
     <div>
-      
+      			{openModal ? <AppModal setOpenModal={setOpenModal} /> : ""}
+
     </div>
   )
 }
